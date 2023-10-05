9a10a95044a14c99498155109282fb86d50f0316
Gaspar <135924893+SO4D@users.noreply.github.com>
2023-10-04 21:07:43 -0300

    Delete RestfakeApplication directory

--- RestfakeApplication/RestfakeApplication/src/main/java/com/facu/restfake/RestfakeApplication/repositories/persona/LocalidadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.facu.restfake.RestfakeApplication.repositories.persona;
-
-import com.facu.restfake.RestfakeApplication.entities.Localidad;
-
-public interface LocalidadRepository extends BaseRepository<Localidad, Long> {
-    
-}
--- RestfakeApplication/RestfakeApplication/src/main/java/com/facu/restfake/RestfakeApplication/services/AutorService.java
+++
@@ -1,6 +0,0 @@
-package com.facu.restfake.RestfakeApplication.services;
-
-import com.facu.restfake.RestfakeApplication.entities.Autor;
-
-public interface AutorService extends BaseService<Autor, Long> {
-}
